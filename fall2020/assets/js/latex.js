856d9e5511ca438fb1d61dcad7110198df200e88
karthik19967829 <karthikganesan17@gmail.com>
2022-09-17 15:20:13 -0400

    refactor

--- fall2020/assets/js/latex.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    document.querySelectorAll("latex-js")
-        .forEach((latexjs) => {
-            var shadow = latexjs.shadowRoot;
-            var page = Array.from(shadow.childNodes)[3];
-            page.style.cssText = "display: block"; 
-        });
-});
